356d4a450e2d509b958632c0d82592e8fc1f7bbb
k019c1077 <46696419+k019c1077@users.noreply.github.com>
2019-02-04 09:47:56 +0900

    条件に合わせました

--- 2/2.ino
+++ 2/2.ino
@@ -7,38 +7,55 @@
 void setup()
 {
     pinMode(LED_PIN, OUTPUT);
-    pinMode(PIN_SW, INPUT);
-    ledcSetup(0,490,8);
-    ledcAttachPin(14,0);
+    pinMode(PIN_SW, INPUT_PULLUP);
+    ledcSetup(0, 490, 8);
+    ledcAttachPin(14, 0);
     Serial.begin(115200);
-
 }
-void loop(){
-    now = digitalRead(PIN_SW);
+void loop()
+{
 
+    in = get_sw(prev); // sw�̏�Ԃ�ǂ�in�ɑ��
 
-    if(last != now){
-        last = now;
-        if(now == HIGH){
-        while(Brightness<256){
-            ledcWrite(0,Brightness);
-            Brightness += 10;
-            delay(Delaytime);
-            
+    if (prev != in)
+    {}
+        prev = in;
+        if (in == HIGH)
+        {
+            while (Brightness < 256)
+            {
+                ledcWrite(0, Brightness);
+                Brightness += 10;
+                delay(Delaytime);
+            }
+            Brightness = 255;
+            ledcWrite(0, Brightness);
         }
-        Brightness = 255;
-        ledcWrite(0,Brightness);
-        }else{
-        while(Brightness>0){
-            ledcWrite(0,Brightness);
-            Brightness -= 10;
-            delay(Delaytime);
-        }
-        Brightness = 0;
-        ledcWrite(0,Brightness);
+        else
+        {
+            while (Brightness > 0)
+            {
+                ledcWrite(0, Brightness);
+                Brightness -= 10;
+                delay(Delaytime);
+            }
+            Brightness = 0;
+            ledcWrite(0, Brightness);
         }
     }
     delay(100);
+}
+int get_sw(int sw_out)
+{
+    int in1, in2;
 
+    in1 = digitalRead(PIN_SW); // GPIO14������� GPIO14��PullUp���Ă���
+    delay(100);                // �v����
+    in2 = digitalRead(PIN_SW);
 
+    if (in1 == in2)
+    {
+        sw_out = in2;
+    }
+    return (sw_out);
 }

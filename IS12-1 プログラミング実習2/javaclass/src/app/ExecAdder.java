687946fbe1be197c3f70d51e2f8db79debc8a4d1
k019c1077 <yukito.azuma16@gmail.com>
2019-05-20 14:12:22 +0900

    処理し直し

--- IS12-1 プログラミング実習2/javaclass/src/app/ExecAdder.java
+++
@@ -1,8 +0,0 @@
-public class ExecAdder {
-	public static void main(String[] args) {
-		Adder num = new Adder(100);
-		num.add();
-		num.add();
-		System.out.println("number=" + num.getNumber());
-	}
-}

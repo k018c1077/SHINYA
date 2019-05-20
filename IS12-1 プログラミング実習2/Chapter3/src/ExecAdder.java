f7e3e106fadcb409f0081c9fbe983c6c495a5281
k018c1052 <k018c1052@KPC-018C1052>
2019-05-20 14:13:23 +0900

    あげなおし

---
+++ IS12-1 プログラミング実習2/Chapter3/src/ExecAdder.java
@@ -0,0 +1,8 @@
+public class ExecAdder {
+	public static void main(String[] args) {
+		Adder num = new Adder(100);
+		num.add();
+		num.add();
+		System.out.println("number=" + num.getNumber());
+	}
+}

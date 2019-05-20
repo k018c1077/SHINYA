e03aed992902ab06658698bc16bea039685aa9c2
k018c1052 <k018c1052@KPC-018C1052>
2019-05-20 23:03:47 +0900

    4,5追加

---
+++ IS12-1 プログラミング実習2/Chapter5/src/GeometricShape.java
@@ -0,0 +1,15 @@
+public class GeometricShape {
+	private String color;
+
+	public GeometricShape(String color) {
+		this.color = color;
+	}
+
+	public String getColor() {
+		return color;
+	}
+
+	public void setColor(String color) {
+		this.color = color;
+	}
+}
---
+++ IS12-1 プログラミング実習2/Chapter5/src/Manager.java
@@ -0,0 +1,16 @@
+public class Manager extends Employee {
+	private String title;
+
+	public Manager(long id, String name, int age, String title) {
+		super(id, name, age);
+		this.title = title;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+}
